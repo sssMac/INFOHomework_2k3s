dfaf975d803a92cf9eee269226cc75e834817d37
Ilya <ilya-lyapin@mail.ru>
2021-12-04 06:58:23 +0300

    HW probability theory

---
+++ ProbabilityTheory.Graph/ProbabilityTheory.Graph/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace ProbabilityTheory.Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_sig = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_P = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_p = new System.Windows.Forms.TextBox();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 12);
+            this.chart.Name = "chart";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series1.LabelForeColor = System.Drawing.Color.Salmon;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(767, 555);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_d);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox_c);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_sig);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_a);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(792, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 276);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paparms";
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.Location = new System.Drawing.Point(85, 212);
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(173, 47);
+            this.textBox_d.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 37);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "d =";
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Location = new System.Drawing.Point(85, 159);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(173, 47);
+            this.textBox_c.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(16, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "c =";
+            // 
+            // textBox_sig
+            // 
+            this.textBox_sig.Location = new System.Drawing.Point(85, 106);
+            this.textBox_sig.Name = "textBox_sig";
+            this.textBox_sig.Size = new System.Drawing.Size(173, 47);
+            this.textBox_sig.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "σ =";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(85, 53);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(173, 47);
+            this.textBox_a.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a =";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(815, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_P
+            // 
+            this.label_P.AutoSize = true;
+            this.label_P.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_P.Location = new System.Drawing.Point(24, 32);
+            this.label_P.Name = "label_P";
+            this.label_P.Size = new System.Drawing.Size(66, 37);
+            this.label_P.TabIndex = 8;
+            this.label_P.Text = "P =";
+            this.label_P.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_p);
+            this.groupBox2.Controls.Add(this.label_P);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.groupBox2.Location = new System.Drawing.Point(785, 420);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(310, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "P";
+            // 
+            // textBox_p
+            // 
+            this.textBox_p.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_p.Location = new System.Drawing.Point(93, 32);
+            this.textBox_p.Name = "textBox_p";
+            this.textBox_p.Size = new System.Drawing.Size(173, 38);
+            this.textBox_p.TabIndex = 8;
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_time.Location = new System.Drawing.Point(877, 526);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(173, 38);
+            this.textBox_time.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 579);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_sig;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_P;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_p;
+        private System.Windows.Forms.TextBox textBox_time;
+    }
+}
+
---
+++ ProbabilityTheory.Graph/ProbabilityTheory.Graph/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProbabilityTheory.Graph
+{
+    public partial class Form1 : Form
+    {
+        private double a, sig, c, d;
+        private double x, y;
+        Stopwatch stopWatch = new Stopwatch();
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopWatch.Start();
+            a = Convert.ToDouble(textBox_a.Text);
+            sig = Convert.ToDouble(textBox_sig.Text);
+            c = Convert.ToDouble(textBox_c.Text);
+            d = Convert.ToDouble(textBox_d.Text);
+
+            var x_start = a - 3 * sig;
+            var x_end = a + 3 * sig;
+
+
+            for (x = x_start; x < x_end; x ++)
+            {
+                y = (1 / (sig * Math.Sqrt(2 * Math.PI))) * (Math.Pow(Math.E, -0.5 * ((x - a) / sig) * ((x - a) / sig)));
+                this.chart.Series[0].Points.AddXY(x, y);
+            }
+
+            
+            DoIntegral(a,sig,c, d);
+            stopWatch.Stop();
+
+            textBox_time.Text = Convert.ToString(stopWatch.Elapsed.TotalMilliseconds);
+
+        }
+
+        private void DoIntegral(double a, double sig, double c, double d)
+        {
+            double f(double x) => Math.Pow(Math.E, ((-x * x) / 2));
+
+            var FI_1 = (c - a) / sig;
+            var FI_2 = (d - a) / sig;
+
+            var FI_1_res = 1 / Math.Sqrt(2 * Math.PI) * Simpson(f, 0, FI_1, 1000);
+            var FI_2_res = 1 / Math.Sqrt(2 * Math.PI) * Simpson(f, 0, FI_2, 1000);
+
+            var res = FI_2_res - FI_1_res;
+
+            textBox_p.Text = Convert.ToString(res);
+        }
+
+        private static double Simpson(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum1 = 0d;
+            var sum2 = 0d;
+            for (var k = 1; k <= n; k++)
+            {
+                var xk = a + k * h;
+                if (k <= n - 1)
+                {
+                    sum1 += f(xk);
+                }
+
+                var xk_1 = a + (k - 1) * h;
+                sum2 += f((xk + xk_1) / 2);
+            }
+
+            var result = h / 3d * (1d / 2d * f(a) + sum1 + 2 * sum2 + 1d / 2d * f(b));
+            return result;
+        }
+    }
+}
